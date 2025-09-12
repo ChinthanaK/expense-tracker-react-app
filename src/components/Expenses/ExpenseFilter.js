732e4701390055c330f7eafe2fd24ea4634945bc
ChinthanaK <chinthanakumar1999>
2025-09-12 10:29:29 +0530

    expensefilter css file missing

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -5,13 +5,15 @@ const ExpenseFilter = (props) => {
         props.onChangeFilter(event.target.value);
     }
   return (
-    <div>
-    <label htmlFor="dropdown">Select the year</label>
-      <select name="" id="dropdown" value={props.onSelected} onChange={dropdownChangeFilter}>
-        <option value="2023">2023</option>
-        <option value="2024">2024</option>
-        <option value="2025">2025</option>
-      </select>
+    <div className='expenses-filter'>
+        <div className='expenses-filter__control'>
+            <label htmlFor="dropdown">Filter by year</label>
+            <select name="" id="dropdown" value={props.onSelected} onChange={dropdownChangeFilter}>
+                <option value="2023">2023</option>
+                <option value="2024">2024</option>
+                <option value="2025">2025</option>
+            </select>
+        </div>
     </div>
   )
 }
